3474dfa113a2a94b8f2fcf91c87ec426be9e8645
B1ackApric0t <apric0t@protonmail.com>
2022-06-20 18:01:30 +0900

    jilogg deleted

---
+++ _nuxt/static/1655715631/hitoyane/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1655715631",layout:"default",error:a,state:{isSoundEnabled:b},serverRendered:b,routePath:"\u002Fhitoyane",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null,true));
