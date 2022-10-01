4bc25bc73b8d59fe1894364c12b18952c402caab
B1ackApric0t <apric0t@protonmail.com>
2022-10-01 22:50:27 +0900

    description updated

---
+++ _nuxt/static/1664632143/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1664632143",layout:"default",error:a,state:{isSoundEnabled:b},serverRendered:b,routePath:c,config:{_app:{basePath:c,assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null,true,"\u002F"));
