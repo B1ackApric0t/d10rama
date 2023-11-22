b0ed993f4bbf6f5b4f53781d38ba12a783cb7234
B1ack Apric0t <apric0t@protonmail.com>
2023-11-22 20:24:14 +0900

    updated news

---
+++ _nuxt/static/1700652178/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002Famehazu","\u002Fmemolily","\u002Fhitoyane","\u002Fnitousei","\u002F","\u002Fwanted"]})
---
+++ _nuxt/static/1700652178/wanted/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1700652178",layout:"default",error:a,state:{isSoundEnabled:b},serverRendered:b,routePath:"\u002Fwanted",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null,true));
